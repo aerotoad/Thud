d303400b62d01f84fce88b924f79dff56bb6c4e5
Samuel Bazaga <samuelbazaga@gmail.com>
2022-03-15 02:09:26 +0100

    Initialize storage on load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from './services/storage/storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+
+  constructor(
+    private storageService: StorageService
+  ) {}
+
+  ngOnInit() {
+    this.storageService.initialize();
+  }
 }
