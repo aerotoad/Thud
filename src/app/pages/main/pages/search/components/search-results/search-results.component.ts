ad92677d22101ea461ca96e30e92b84957e41ddf
Samuel Bazaga <samuelbazaga@gmail.com>
2022-03-15 17:40:55 +0100

    Pass searchQuery to results component

--- src/app/pages/main/pages/search/components/search-results/search-results.component.ts
+++ src/app/pages/main/pages/search/components/search-results/search-results.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { FeedlyService } from 'src/app/services/feedly/feedly.service';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
 })
-export class SearchResultsComponent implements OnInit {
+export class SearchResultsComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() searchQuery: string;
+
+  constructor(
+    private feedlyService: FeedlyService
+  ) { }
 
   ngOnInit() {}
+
+  ngOnChanges() {
+    if (this.searchQuery) {
+      this.feedlyService.search(this.searchQuery);
+    }
+  }
 
 }
