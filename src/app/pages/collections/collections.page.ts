ade6779eebdbf2ee085513212f5cc4fede66a946
Samuel Bazaga <samuelbazaga@gmail.com>
2022-03-18 21:59:52 +0100

    Add edition and deletion capabilities

--- src/app/pages/collections/collections.page.ts
+++ src/app/pages/collections/collections.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { AlertController, ModalController } from '@ionic/angular';
 import { AddCollectionComponent } from 'src/app/components/add-collection/add-collection.component';
 import Collection from 'src/app/models/Collection';
 import { StorageService } from 'src/app/services/storage/storage.service';
@@ -15,7 +15,8 @@ export class CollectionsPage {
 
   constructor(
     private storageService: StorageService,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private alertCtrl: AlertController
   ) { }
 
   ionViewWillEnter() {
@@ -33,11 +34,69 @@ export class CollectionsPage {
     });
     modal.onDidDismiss()
       .then((data) => {
-        if (data.data.collection) {
+        if (data.data?.collection) {
           this.collections.push(data.data.collection);
         }
       });
     await modal.present();
   }
+
+  async editCollection(collection: Collection) {
+    const modal = await this.modalCtrl.create({
+      component: AddCollectionComponent,
+      cssClass: 'add-collection-modal',
+      componentProps: {
+        collection
+      }
+    });
+    modal.onDidDismiss()
+      .then((data) => {
+        if (data.data?.collection) {
+          this.collections = this.collections.map((collection: Collection) => {
+            if (collection.id === data.data.collection.id) {
+              return data.data.collection;
+            }
+            return collection;
+          });
+        }
+      });
+    await modal.present();
+  }
+
+  async confirmDeleteCollection(collection: Collection) {
+    const alert = await this.alertCtrl.create({
+      header: 'Delete collection',
+      message: `Are you sure you want to delete the collection "${collection.name}"?`,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Delete',
+          cssClass: 'alert-danger-button',
+          handler: () => {
+            this.deleteCollection(collection);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async deleteCollection(collection: Collection) {
+    // Cleanup collection feed cache
+    const feedIds = collection.feedList.map(feedId => feedId.feedId);
+    feedIds.forEach(async (feedId) => {
+      await this.storageService.deleteCacheByFeedId(feedId);
+    });
+
+    // Remove collection from storage
+    this.collections = this.collections.filter((currentCollection: Collection) => {
+      if (currentCollection.id !== collection.id) return currentCollection;
+    });
+    await this.storageService.deleteCollection(collection);
+  }
+
 
 }
