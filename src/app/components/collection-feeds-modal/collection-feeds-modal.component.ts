89b115b0920678aab81eedddbc18e8f591aedf39
Samuel Bazaga <samuelbazaga@gmail.com>
2022-03-19 00:58:10 +0100

    Create collection feeds modal

---
+++ src/app/components/collection-feeds-modal/collection-feeds-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-feeds-modal',
+  templateUrl: './collection-feeds-modal.component.html',
+  styleUrls: ['./collection-feeds-modal.component.scss'],
+})
+export class CollectionFeedsModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
