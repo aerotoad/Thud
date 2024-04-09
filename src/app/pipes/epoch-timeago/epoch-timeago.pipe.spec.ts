25a7d56259960411a2613aa6c44b499a52a755fc
samuel bazaga <samuelbazaga@gmail.com>
2024-04-09 17:08:59 +0200

    chore: Remove tests files

--- src/app/pipes/epoch-timeago/epoch-timeago.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EpochTimeagoPipe } from './epoch-timeago.pipe';
-
-describe('EpochTimeagoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EpochTimeagoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/pipes/order-by-index/order-by-index.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OrderByIndexPipe } from './order-by-index.pipe';
-
-describe('OrderByIndexPipe', () => {
-  it('create an instance', () => {
-    const pipe = new OrderByIndexPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
